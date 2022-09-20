e6c93b2fd924f633ec8c56efb9add7fbe4b5b93a
charlescianos <charlescianos@penguin>
2022-09-20 00:44:05 -0700

    Adding function.

--- nim_via_graphics/hzvtline_len_dir.nim
+++ nim_via_graphics/hzvtline_len_dir.nim
@@ -9,6 +9,11 @@ proc drawHorizantalLine(start: Point, length: Positive) =
   for delta in 0 .. length:
     putPixel(start.x + delta, start.y)
 
+proc drawVerticalLine(start: Point, length: Positive) =
+  for delta in 0 .. length:
+    putPixel(start.x, start.y + delta)
+
 let a = Point(x: 60, y: 40)
 
 drawHorizantalLine(a, 50)
+drawVerticalLine(a, 50)
